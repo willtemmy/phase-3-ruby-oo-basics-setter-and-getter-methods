86bf9a65c10052159e864f876c12a7fcbb23d828
William Mtemi Mponzi <willbrian009@gmail.com>
2023-05-23 23:28:19 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    # setter method
+  def name=(name)
+    @name = name
+  end
 
+  # getter method
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+     # setter method
+  def name=(name)
+    @name = name
+  end
 
+  # getter method
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
 end
